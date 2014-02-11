ad66ad7e65ffa69234725e4dc5e1abc16c0466cb
Megacrafter127 <player4596@web.de>
2014-02-11 18:59:14 +0100

    fixed unchanged texture of DimInfuser

--- src/com/matt/mod/blocks/BlockDimensionalTransmutator.java
+++ src/com/matt/mod/blocks/BlockDimensionalTransmutator.java
@@ -1,21 +1,36 @@
 package com.matt.mod.blocks;
 
+
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.biome.*;
+import net.minecraft.util.Icon;
 
 import com.matt.lib.Ref;
 import com.matt.mod.blocks.tileentity.TileEntityDimensionalInfuser;
 
 public class BlockDimensionalTransmutator extends Block implements ITileEntityProvider {
+	private Icon inactive;
+	private Icon nether;
+	private Icon end;
+	
 	public BlockDimensionalTransmutator(int par1) {
 		super(par1, Material.iron);
 		setTextureName(Ref.NAME.toLowerCase() + ":diminfuser");
 		
+	}
+	
+	@Override
+	public void registerIcons(IconRegister r) {
+		inactive=r.registerIcon(Ref.NAME.toLowerCase() + ":diminfuser");
+		nether=r.registerIcon(Ref.NAME.toLowerCase() + ":diminfusernether");
+		end=r.registerIcon(Ref.NAME.toLowerCase() + ":diminfuserend");
 	}
 	@Override
 	public boolean onBlockActivated(World world, int x, int y, int z,
@@ -28,11 +43,11 @@ public class BlockDimensionalTransmutator extends Block implements ITileEntityPr
 	}
 	
 	@Override
-	public void onBlockAdded(World w,int x,int y,int z) {
-		BiomeGenBase b=w.getBiomeGenForCoords(x, z);
-		if(b instanceof BiomeGenEnd) setTextureName(Ref.NAME.toLowerCase() + ":diminfuserend");
-		else if(b instanceof BiomeGenHell) setTextureName(Ref.NAME.toLowerCase() + ":diminfusernether");
-		else setTextureName(Ref.NAME.toLowerCase() + ":diminfuser");
+	public Icon getBlockTexture(IBlockAccess a,int x,int y,int z,int side) {
+		BiomeGenBase b=a.getBiomeGenForCoords(x, z);
+		if(b instanceof BiomeGenHell) return nether;
+		if(b instanceof BiomeGenEnd) return end;
+		return inactive;
 	}
 	
 	@Override
