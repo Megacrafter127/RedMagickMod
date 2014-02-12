b9c8da62a2c4d491fa4bf7f400b64767e22c763f
Matheus <matheus.de.alcantara@gmail.com>
2014-02-12 16:05:11 -0200

    Added stand-alone helper for registering everything.

---
+++ src/com/matt/mod/ModHelper.java
@@ -0,0 +1,171 @@
+package com.matt.mod;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+
+import com.matt.FutureCraft;
+import com.matt.lib.Ref;
+import com.matt.mod.blocks.BlockDimensionalInfuser;
+import com.matt.mod.blocks.BlockFutureBasic;
+import com.matt.mod.blocks.BlockFutureFurnace;
+import com.matt.mod.blocks.BlockFutureGold;
+import com.matt.mod.blocks.BlockOre;
+import com.matt.mod.blocks.BlockPowerPipe;
+import com.matt.mod.blocks.tileentity.TileEntityPowerPipe;
+import com.matt.mod.blocks.tileentity.TileEntitySmelter;
+import com.matt.mod.handlers.FutureCraftRegistry;
+import com.matt.mod.item.ItemEnderium;
+import com.matt.mod.item.ItemModPickaxe;
+import com.matt.mod.item.ItemNetherium;
+import com.matt.mod.recipes.PickRecipe;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+
+public class ModHelper {
+	public static ModHelper instance;
+	public static ModHelper getInstance() {
+		return instance;
+		
+	}
+	//Blocks
+	public static final Block basicFutureBlock = new BlockFutureBasic(FutureCraft.basicFBlockID, Material.ground,"blueblockfuture");
+	public static final Block goldenFutureBlock = new BlockFutureGold(FutureCraft.goldenFBlockID, Material.ground,"goldfutureblock");
+	public static final Block basicFutureFurnace = new BlockFutureFurnace(FutureCraft.basicFurnaceID,"compressionSmelter");
+	public static final Block blockPowerPipe = new BlockPowerPipe(FutureCraft.powerGenPassiveID,"powerpipe");
+	public static final Block dimensionalInfuser = new BlockDimensionalInfuser(FutureCraft.dimTransID);
+	public static final Block oreIndium = new BlockOre(FutureCraft.oreIndiumID,"oreindium").setTextureName(Ref.NAME.toLowerCase() + ":oreindium");
+	public static final Block oreRoent = new BlockOre(FutureCraft.oreRoentgeniumID,"oreroentgenium").setTextureName(Ref.NAME.toLowerCase() + ":oreroentgenium");
+	public static void registerBlocksInForge() {
+		GameRegistry.registerBlock(basicFutureBlock, "basicFutureBlock");
+		GameRegistry.registerBlock(goldenFutureBlock, "goldenFutureBlock");
+		GameRegistry.registerBlock(basicFutureFurnace,"basicFutureFurnace");
+		GameRegistry.registerBlock(blockPowerPipe,"blockPowerPipe");
+		GameRegistry.registerBlock(oreIndium, "oreIndium");
+		GameRegistry.registerBlock(oreRoent, "oreRoentgenium");
+		GameRegistry.registerBlock(dimensionalInfuser, "dimensionalInfuser");
+		}
+	public static void registerBlockNames() {
+		LanguageRegistry.addName(basicFutureBlock, "Iron Machine Handler");
+		LanguageRegistry.addName(goldenFutureBlock, "Golden Machine Handler");
+		LanguageRegistry.addName(basicFutureFurnace, "Basic Compression Smelter");
+		LanguageRegistry.addName(blockPowerPipe, "Power Generator ( Passive ) ");
+		FutureCraftRegistry.setLocalizedName("en_US", oreIndium, "Indium Ore");
+		FutureCraftRegistry.setLocalizedName("en_US", oreRoent, "Roentgenium Ore");
+		FutureCraftRegistry.setLocalizedName("en_UK", oreIndium, "Indium Ore");
+		FutureCraftRegistry.setLocalizedName("en_UK", oreRoent, "Roentgenium Ore");
+		LanguageRegistry.addName(dimensionalInfuser, "Dimensional Infuser");
+	}
+	public static void registerMiningTools() {
+		MinecraftForge.setBlockHarvestLevel(basicFutureBlock, "pickaxe",1);
+		MinecraftForge.setBlockHarvestLevel(goldenFutureBlock, "pickaxe",1);
+		MinecraftForge.setBlockHarvestLevel(basicFutureFurnace, "pickaxe",1);
+		MinecraftForge.setBlockHarvestLevel(oreIndium, "pickaxe",2);
+		MinecraftForge.setBlockHarvestLevel(oreRoent, "pickaxe",3);
+		
+	}
+	public static void setCreativeTabs() {
+		blockPowerPipe.setCreativeTab(Ref.getRecommendedTab("block"));
+		basicFutureBlock.setCreativeTab(Ref.getRecommendedTab("block"));
+		goldenFutureBlock.setCreativeTab(Ref.getRecommendedTab("block"));
+		basicFutureFurnace.setCreativeTab(Ref.getRecommendedTab("block"));
+		oreIndium.setCreativeTab(Ref.getRecommendedTab("block"));
+		oreRoent.setCreativeTab(Ref.getRecommendedTab("block"));
+		dimensionalInfuser.setCreativeTab(Ref.getRecommendedTab("block"));
+	}
+	public static void registerBlocks() {
+		registerBlocksInForge();
+		registerBlockNames();
+		setCreativeTabs();
+	}
+	public static ItemNetherium neth = new ItemNetherium(FutureCraft.netheriumIngotId,"ingotNetherium", CreativeTabs.tabMisc, "netheriumingot");
+	public static ItemEnderium endh = new ItemEnderium(FutureCraft.enderiumIngotId,"ingotEnderium", CreativeTabs.tabMisc, "enderiumingot");
+	public static Item ingotIndium = new ItemEnderium(FutureCraft.ingotIndiumId,"ingotIndium", CreativeTabs.tabMisc, "indiumtingot").setTextureName(Ref.NAME.toLowerCase() +":indiumingot");
+	public static Item ingotRoentgenium = new ItemEnderium(FutureCraft.ingotRoentgeniumId,"ingotRoentgenium", CreativeTabs.tabMisc, "roentgeniumingot").setTextureName(Ref.NAME.toLowerCase() +":roentgeniumingot");
+	public static final ItemModPickaxe nethpick=new ItemModPickaxe(FutureCraft.netheriumPickaxeId,ItemModPickaxe.netherium);
+	public static final ItemModPickaxe endpick=new ItemModPickaxe(FutureCraft.enderiumPickaxeId,ItemModPickaxe.enderium);
+	public static final ItemModPickaxe indiumPick=new ItemModPickaxe(FutureCraft.indiumPickaxeId,ItemModPickaxe.indium);
+	public static void registerAllItems() {
+		GameRegistry.registerItem(neth, "ingotNetherium");
+		
+		GameRegistry.registerItem(endh, "ingotEnderium");
+		
+		GameRegistry.registerItem(ingotIndium, "ingotIndium");
+		
+		GameRegistry.registerItem(ingotIndium, "ingotRoentgenium");
+		
+		GameRegistry.registerItem(nethpick, "pickaxeNetherium");
+		
+		GameRegistry.registerItem(endpick, "pickaxeEnderium");
+		
+		
+	}
+	public static void registerItemNames() {
+		LanguageRegistry.addName(neth, "Netherium Ingot");
+		LanguageRegistry.addName(endh, "Enderium Ingot");
+		LanguageRegistry.addName(ingotIndium, "Indium Ingot");
+		LanguageRegistry.addName(ingotRoentgenium, "Roentgenium Ingot");
+		LanguageRegistry.addName(nethpick, "Netherium Pickaxe");
+		LanguageRegistry.addName(endpick, "Enderium Pickaxe");
+	}
+	public static void registerItemCreativeTabs() {
+		neth.setCreativeTab(Ref.getRecommendedTab("block"));
+		endh.setCreativeTab(Ref.getRecommendedTab("block"));
+		ingotIndium.setCreativeTab(Ref.getRecommendedTab("block"));
+		ingotRoentgenium.setCreativeTab(Ref.getRecommendedTab("block"));
+		nethpick.setCreativeTab(Ref.getRecommendedTab("block"));
+		endpick.setCreativeTab(Ref.getRecommendedTab("block"));
+	}
+	//Recipes
+	public static ItemStack dirtStack = new ItemStack(Block.dirt);
+	public static ItemStack seedStack = new ItemStack(Item.seeds);
+	public  static ItemStack grasStack = new ItemStack(Block.grass);
+	public static ItemStack ironStack = new ItemStack(Item.ingotIron,1);
+	public static ItemStack diamondStack = new ItemStack(Item.diamond,1);
+	public static ItemStack goldStack = new ItemStack(Item.ingotGold,1);
+	public static ItemStack bFutureStackCraft = new ItemStack(getInstance().basicFutureBlock,8);
+	public static ItemStack gFutureStackCraft = new ItemStack(getInstance().goldenFutureBlock,8);
+	public static ItemStack bFutureStack = new ItemStack(getInstance().basicFutureBlock);
+	public static ItemStack gFutureStack = new ItemStack(getInstance().goldenFutureBlock);
+	
+	public static final ItemStack stickStack = new ItemStack(Item.stick);
+	public static final ItemStack enderpearlStack = new ItemStack(Item.enderPearl);
+	public static final ItemStack enderiumStack = new ItemStack(getInstance().endh);
+	public static final ItemStack netheriumStack = new ItemStack(getInstance().neth);
+	public static final ItemStack indiumStack = new ItemStack(getInstance().ingotIndium);
+	public static final ItemStack roentgeniumStack = new ItemStack(getInstance().ingotRoentgenium);
+	
+	public static void registerRecipesShapeless() {
+		GameRegistry.addShapelessRecipe(grasStack,dirtStack, seedStack);
+	}
+	public static void registerRecipesShaped() {
+		GameRegistry.addRecipe(bFutureStackCraft, 
+     			"xxx", "xyx", "xxx", 
+     			'x',ironStack,'y',diamondStack);
+		GameRegistry.addRecipe(gFutureStackCraft, 
+     			"xzx", "zyz", "xzx", 
+     			'x',goldStack,'y',diamondStack, 'z', enderiumStack );
+		GameRegistry.addRecipe(new ItemStack(getInstance().dimensionalInfuser,1),"xxx","xyx","xxx",'x',ironStack,'y',enderpearlStack);
+		GameRegistry.addRecipe(new PickRecipe(enderiumStack,stickStack,new ItemStack(getInstance().endpick,1)));
+		GameRegistry.addRecipe(new PickRecipe(netheriumStack,stickStack,new ItemStack(getInstance().nethpick,1)));
+		GameRegistry.addRecipe(new PickRecipe(indiumStack,stickStack,new ItemStack(getInstance().indiumPick,1)));
+	}
+	//Tile entities
+	public static void registerTileEntities() {
+		GameRegistry.registerTileEntity(TileEntitySmelter.class, "tileEntitySmelter");
+		GameRegistry.registerTileEntity(TileEntityPowerPipe.class, "tileeEntityPowerPipe");
+	}
+	public static void registerAll() {
+		registerBlocks();
+		registerItemNames();
+		registerItemCreativeTabs();
+		registerAllItems();
+		registerRecipesShapeless();
+		registerRecipesShaped();
+		
+	}
+}
