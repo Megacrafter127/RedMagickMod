655dbdd6dd9e6dcdc51050be9a9ba3633271a6e2
Matheus <matheus.de.alcantara@gmail.com>
2014-02-13 17:40:02 -0200

    Fixed derp. Added memorial DERP tag.

--- src/com/matt/proxy/client/ClientProxy.java
+++ src/com/matt/proxy/client/ClientProxy.java
@@ -4,6 +4,7 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
 import com.matt.gui.LoreBookGui;
+import com.matt.mod.ModHelper;
 import com.matt.proxy.CommonProxy;
 
 public class ClientProxy extends CommonProxy {
@@ -12,16 +13,20 @@ public class ClientProxy extends CommonProxy {
         public void registerRenderers() {
                 // This is for rendering entities and so forth later on
         }
+        //DERP : HOW DID I MESS THIS UP!?
         @Override
         public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
         {
+        	if(player.inventory.getCurrentItem().itemID == ModHelper.loreBook.itemID && ID == 0) {
+        		return new LoreBookGui();
+        	}
         	TileEntity tileEntity = world.getBlockTileEntity(x, y, z);
         	if (tileEntity != null)
         	{
         	switch(ID)
         	{
         		case 0: /* your GUIs go here */
-        			return new LoreBookGui();
+        			
         	}
         	}
         	return null;
