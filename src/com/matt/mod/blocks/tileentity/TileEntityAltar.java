1ac7ed2883186c44234419865352bf858f3387ce
Matheus <matheus.de.alcantara@gmail.com>
2014-02-13 18:30:40 -0200

    COMMITING ALL THE THINGS!

---
+++ src/com/matt/mod/blocks/tileentity/TileEntityAltar.java
@@ -0,0 +1,9 @@
+package com.matt.mod.blocks.tileentity;
+
+import net.minecraft.tileentity.TileEntity;
+
+import com.matt.heneric.helpers.IFutureCraft;
+
+public class TileEntityAltar extends TileEntity implements IFutureCraft {
+
+}
