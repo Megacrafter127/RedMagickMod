ea472d290687cdcc8ecf909eafaa160d334708f5
Matheus <matheus.de.alcantara@gmail.com>
2014-02-14 18:42:12 -0200

    Finished work on catalist!

--- src/com/matt/mod/swag/items/ItemFlyingCatalist.java
+++ src/com/matt/mod/swag/items/ItemFlyingCatalist.java
@@ -1,5 +1,22 @@
 package com.matt.mod.swag.items;
 
-public class ItemFlyingCatalist {
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
+import com.matt.FutureCraft;
+import com.matt.lib.Ref;
+import com.matt.mod.swag.items.logic.FlyingCatalistLogic;
+
+public class ItemFlyingCatalist extends Item{
+public ItemFlyingCatalist() {
+	super(FutureCraft.swag_catalist_id);
+	setTextureName(Ref.NAME.toLowerCase() + ":catalist1");
+}
+@Override
+public boolean onItemUse(ItemStack itemStack, EntityPlayer entityPlayer, World world, int x, int y, int z, int sideHit, float hitVecX, float hitVecY, float hitVecZ) {
+	return FlyingCatalistLogic.run(entityPlayer);
+	
+}
 }
