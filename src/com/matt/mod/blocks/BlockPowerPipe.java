5ce1aaa031dfb1ef8dd41392edf9ec5122da79ca
Megacrafter127 <player4596@web.de>
2014-02-14 15:12:30 +0100

    added ticking and ticking debug output

--- src/com/matt/mod/blocks/BlockPowerPipe.java
+++ src/com/matt/mod/blocks/BlockPowerPipe.java
@@ -1,5 +1,6 @@
 package com.matt.mod.blocks;
 
+import java.util.Random;
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
@@ -23,5 +24,19 @@ public class BlockPowerPipe extends Block implements ITileEntityProvider{
 	public TileEntity createNewTileEntity(World world) {
 		return new TileEntityPowerPipe();
 	}
-
+	
+	@Override
+	public void updateTick(World w,int x,int y,int z,Random r) {
+		System.out.println("tick called:");
+		try{
+			((TileEntityPowerPipe)w.getBlockTileEntity(x, y, z)).run(w);
+			System.out.println("	ticked powerpipe.");
+		}
+		catch(ClassCastException ex) {}
+	}
+	
+	@Override
+	public int tickRate(World w) {
+		return 1;
+	}
 }
