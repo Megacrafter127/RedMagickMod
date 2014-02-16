0be4ee2865a8d693591f03d06d8a81a131f3fa4a
Megacrafter127 <player4596@web.de>
2014-02-16 21:57:20 +0100

    made particles more attractive

--- src/com/matt/mod/kernelcraft/tileentities/TileEntityKernelCore.java
+++ src/com/matt/mod/kernelcraft/tileentities/TileEntityKernelCore.java
@@ -8,6 +8,8 @@ import net.minecraft.tileentity.TileEntityBeacon;
 import net.minecraft.world.World;
 
 public class TileEntityKernelCore extends TileEntityBeacon {
+	private int tick;
+	private static final int maxTick=10;
 	private static class StraightEnchant extends EntityEnchantmentTableParticleFX {
 
 		public StraightEnchant(World par1World, double par2, double par4,
@@ -48,14 +50,19 @@ public class TileEntityKernelCore extends TileEntityBeacon {
 	}
 	@Override
 	public void updateEntity() {
-		Minecraft.getMinecraft().effectRenderer.addEffect(new StraightEnchant(getWorldObj(),xCoord+0.5,yCoord+1,zCoord+0.5,0,0.1,0));
-		Minecraft.getMinecraft().effectRenderer.addEffect(new StraightEnchant(getWorldObj(),xCoord+0.5,yCoord-1,zCoord+0.5,0,-0.1,0));
-		
-		Minecraft.getMinecraft().effectRenderer.addEffect(new StraightEnchant(getWorldObj(),xCoord+1,yCoord+0.5,zCoord+0.5,0.1,0,0));
-		Minecraft.getMinecraft().effectRenderer.addEffect(new StraightEnchant(getWorldObj(),xCoord,yCoord+0.5,zCoord+0.5,-0.1,0,0));
-		
-		Minecraft.getMinecraft().effectRenderer.addEffect(new StraightEnchant(getWorldObj(),xCoord+0.5,yCoord+0.5,zCoord+1,0,0,0.1));
-		Minecraft.getMinecraft().effectRenderer.addEffect(new StraightEnchant(getWorldObj(),xCoord+0.5,yCoord+0.5,zCoord,0,0,-0.1));
+		tick++;
+		if(tick>maxTick) {
+			tick=0;
+			Minecraft.getMinecraft().effectRenderer.addEffect(new StraightEnchant(getWorldObj(),xCoord+0.5,yCoord+1,zCoord+0.5,0,0.1,0));
+			
+			Minecraft.getMinecraft().effectRenderer.addEffect(new StraightEnchant(getWorldObj(),xCoord+0.5,yCoord-1,zCoord+0.5,0,-0.1,0));
+			
+			Minecraft.getMinecraft().effectRenderer.addEffect(new StraightEnchant(getWorldObj(),xCoord+1,yCoord+0.5,zCoord+0.5,0.1,0,0));
+			Minecraft.getMinecraft().effectRenderer.addEffect(new StraightEnchant(getWorldObj(),xCoord,yCoord+0.5,zCoord+0.5,-0.1,0,0));
+			
+			Minecraft.getMinecraft().effectRenderer.addEffect(new StraightEnchant(getWorldObj(),xCoord+0.5,yCoord+0.5,zCoord+1,0,0,0.1));
+			Minecraft.getMinecraft().effectRenderer.addEffect(new StraightEnchant(getWorldObj(),xCoord+0.5,yCoord+0.5,zCoord,0,0,-0.1));
+		}
 		
 		double d=Math.random()*2*Math.PI;
 		double d2=Math.random()*Math.PI/2;
@@ -78,7 +85,8 @@ public class TileEntityKernelCore extends TileEntityBeacon {
 				y=startcoords[0];
 				z=startcoords[1]*(i%2==0?1:-1);
 			}
-			Minecraft.getMinecraft().effectRenderer.addEffect(new StraightEnchant(getWorldObj(),xCoord+x,yCoord+y,zCoord+z,x*0.05,y*0.05,z*0.05,(int)Math.round(distance)*20));
+			Minecraft.getMinecraft().effectRenderer.addEffect(new StraightEnchant(getWorldObj(),xCoord+0.5,yCoord+0.5,zCoord+0.5,x*0.05,y*0.05,z*0.05,(int)Math.round(distance)*20));
+			Minecraft.getMinecraft().effectRenderer.addEffect(new StraightEnchant(getWorldObj(),xCoord+x+0.5,yCoord+y+0.5,zCoord+z+0.5,-x*0.05,-y*0.05,-z*0.05,(int)Math.round(distance)*20));
 		}
 	}
 	@Override
