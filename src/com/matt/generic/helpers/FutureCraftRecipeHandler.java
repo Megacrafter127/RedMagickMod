bdd65b850ddc8acabae71ae825ffceeb0cffd2ef
Matheus <matheus.de.alcantara@gmail.com>
2014-02-17 17:38:47 -0300

    Finshed recipe? Maybe, maybe not!

--- src/com/matt/generic/helpers/FutureCraftRecipeHandler.java
+++ src/com/matt/generic/helpers/FutureCraftRecipeHandler.java
@@ -8,16 +8,40 @@ import net.minecraft.item.Item;
 import com.matt.mod.swag.infusion.InfusionRecipe;
 
 public class FutureCraftRecipeHandler {
+	 FutureCraftRecipeHandler instance = new  FutureCraftRecipeHandler();
 private static HashMap<Block,Item> smelterRecipe = new HashMap<Block,Item>();
 private static HashMap<Block,Item> crusherRecipe = new HashMap<Block,Item>();
-private static HashMap<InfusionRecipe,Item> infusionRecipes = new HashMap<InfusionRecipe,Item>();
+private static InfusionRecipe[] infusionRecipes = new InfusionRecipe[100000];
 public static HashMap getSmelterRecipes() {
 	return smelterRecipe;
 }
 public static HashMap getCrusherRecipe() {
 	return crusherRecipe;
 }
-public static HashMap getInfusion() {
+public static InfusionRecipe[] getInfusion() {
 	return infusionRecipes;
 }
+public FutureCraftRecipeHandler instance() {
+	return instance;
+}
+public void registerSmelterRecipe(Block b, Item i) {
+	if(b != null) {
+		if(i != null) {
+			smelterRecipe.put(b,i);
+		}
+	}
+}
+public void registerCrusherRecipe(Block b, Item i) {
+	if(b != null) {
+		if(i != null) {
+			crusherRecipe.put(b,i);
+		}
+	}
+}
+public void registerInfusionRecipe(Block[] b, Item i[]) {
+	InfusionRecipe ir = new InfusionRecipe(b,i);
+ 		if(ir != null) {
+	 	infusionRecipes[infusionRecipes.length + 1] = ir;
+ 		}
+	}
 }
