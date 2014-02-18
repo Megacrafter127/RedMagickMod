c8632496d68360382a528dcabb1c928c097b340e
Megacrafter127 <player4596@web.de>
2014-02-18 07:20:23 +0100

    fixed some of matts derps; marked unneccessary methods as deprecated

--- src/com/matt/generic/helpers/FutureCraftRecipeHandler.java
+++ src/com/matt/generic/helpers/FutureCraftRecipeHandler.java
@@ -8,40 +8,50 @@ import net.minecraft.item.Item;
 import com.matt.mod.swag.infusion.InfusionRecipe;
 
 public class FutureCraftRecipeHandler {
-	 FutureCraftRecipeHandler instance = new  FutureCraftRecipeHandler();
-private static HashMap<Block,Item> smelterRecipe = new HashMap<Block,Item>();
-private static HashMap<Block,Item> crusherRecipe = new HashMap<Block,Item>();
-private static InfusionRecipe[] infusionRecipes = new InfusionRecipe[100000];
-public static HashMap getSmelterRecipes() {
-	return smelterRecipe;
-}
-public static HashMap getCrusherRecipe() {
-	return crusherRecipe;
-}
-public static InfusionRecipe[] getInfusion() {
-	return infusionRecipes;
-}
-public FutureCraftRecipeHandler instance() {
-	return instance;
-}
-public void registerSmelterRecipe(Block b, Item i) {
-	if(b != null) {
-		if(i != null) {
-			smelterRecipe.put(b,i);
-		}
-	}
-}
-public void registerCrusherRecipe(Block b, Item i) {
-	if(b != null) {
-		if(i != null) {
-			crusherRecipe.put(b,i);
-		}
-	}
-}
-public void registerInfusionRecipe(Block[] b, Item i[]) {
-	InfusionRecipe ir = new InfusionRecipe(b,i);
- 		if(ir != null) {
-	 	infusionRecipes[infusionRecipes.length + 1] = ir;
- 		}
-	}
+	 public static final FutureCraftRecipeHandler instance = new  FutureCraftRecipeHandler();
+	 private HashMap<Block,Item> smelterRecipe = new HashMap<Block,Item>();
+	 private HashMap<Block,Item> crusherRecipe = new HashMap<Block,Item>();
+	 private InfusionRecipe[] infusionRecipes = new InfusionRecipe[100000];
+	 public HashMap getSmelterRecipes() {
+		 return smelterRecipe;
+	 }
+	 public HashMap getCrusherRecipe() {
+		 return crusherRecipe;
+	 }
+	 public InfusionRecipe[] getInfusion() {
+		 return infusionRecipes;
+	 }
+	 /**
+	  * the variable, this method returns is public and final, this method makes no sense.
+	  * @return the standard FutureCraftRecipeHandler
+	  */
+	 @Deprecated
+	 public static FutureCraftRecipeHandler instance() {
+		 return instance;
+	 }
+	 public void registerSmelterRecipe(Block b, Item i) {
+		 if(b != null) {
+			 if(i != null) {
+				 smelterRecipe.put(b,i);
+			 }
+		 }
+	 }
+	 public void registerCrusherRecipe(Block b, Item i) {
+		 if(b != null) {
+			 if(i != null) {
+				 crusherRecipe.put(b,i);
+			 }
+		 }
+	 }
+	 public void registerInfusionRecipe(Block[] b, Item i[]) {
+		 InfusionRecipe ir = new InfusionRecipe(b,i);
+ 			//if(ir != null) { ir cannot be null
+ 				//infusionRecipes[infusionRecipes.length + 1] = ir; will always throw ArrayIndexOutOfBoundsException
+ 				
+ 			//}
+		 InfusionRecipe[] buffer=infusionRecipes;//the array should be buffered to prevent data loss while extending
+		 infusionRecipes=new InfusionRecipe[buffer.length+1];//extending array length by one
+		 System.arraycopy(buffer, 0, infusionRecipes, 0, buffer.length);//copying data to extended array
+		 infusionRecipes[buffer.length]=ir;//add recipe to the array
+	 }
 }
