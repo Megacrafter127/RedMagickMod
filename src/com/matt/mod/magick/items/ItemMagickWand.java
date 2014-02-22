786078b033bbe3a8dffcb45ebc94dc819cbd0859
Matheus <matheus.de.alcantara@gmail.com>
2014-02-22 14:22:31 -0300

    NBT!!

--- src/com/matt/mod/magick/items/ItemMagickWand.java
+++ src/com/matt/mod/magick/items/ItemMagickWand.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.Icon;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
@@ -179,6 +181,7 @@ public class ItemMagickWand extends Item {
 	        public boolean hasEffect(ItemStack par1ItemStack) {
 	        	return true;
 	        }
+	        /*Deprecated
 	        @Override
 	        public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int x, int y, int z, int par7, float par8, float par9, float par10)
 	        {
@@ -191,8 +194,8 @@ public class ItemMagickWand extends Item {
 	        	}catch(Throwable t) {
 	        		t.printStackTrace();
 	        		return false;
-	        	} */
-	        }	      
+	        	} *
+	        }	    */  
 	        @Deprecated
 	        public  void writeToNBT(ItemStack par1ItemStack) { 
 	        	if(par1ItemStack.stackTagCompound.getTag("charge") != null) {
@@ -213,4 +216,64 @@ public class ItemMagickWand extends Item {
 	        		currentCharge = par1ItemStack.stackTagCompound.getInteger("charge");
 	        	}
 	        }
-}
+	      //called when the item is used (right click) return true if something happens, false if nothing happens (if something happens the animation will trigger)
+	        public boolean onItemUse(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10) {
+				return false; 
+	        	
+	        }
+
+	        /**
+	             * Called each tick as long the item is on a player inventory. Uses by maps to check if is on a player hand and
+	             * update it's contents.
+	             */
+	            public void onUpdate(ItemStack par1ItemStack, World par2World, Entity par3Entity, int par4, boolean par5) {
+	            	par1ItemStack.stackTagCompound = new NBTTagCompound();
+	            }
+
+	        /**
+	             * Called when item is crafted/smelted. Used only by maps so far.
+	             */
+	            public void onCreated(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer) {
+	            	  par1ItemStack.stackTagCompound = new NBTTagCompound();
+	            	  par1ItemStack.stackTagCompound.setInteger("chargeLvl",getCharge(this.getDamage(par3EntityPlayer.inventory.getCurrentItem())));
+	            	  
+	            }
+
+	        /**
+	             * called when the player releases the use item button. Args: itemstack, world, entityplayer, itemInUseCount
+	             */
+	            public void onPlayerStoppedUsing(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer, int par4) {}
+
+	        /**
+	             * allows items to add custom lines of information to the mouseover description
+	             */
+	            public void addInformation(ItemStack itemStack, EntityPlayer player,
+                        List list, boolean par4) {
+	            	   if (itemStack.stackTagCompound != null) { 
+	            		   list.add("Current owner :  " + player.getDisplayName());
+	            	   }
+	            }
+
+	        /**
+	             * Returns true if players can use this item to affect the world (e.g. placing blocks, placing ender eyes in portal)
+	             * when not in creative
+	             */
+	            public boolean canItemEditBlocks() {
+					return false; }
+
+	        /**
+	             * This is called when the item is used, before the block is activated.
+	             * @param stack The Item Stack
+	             * @param player The Player that used the item
+	             * @param world The Current World
+	             * @param x Target X Position
+	             * @param y Target Y Position
+	             * @param z Target Z Position
+	             * @param side The side of the target hit
+	             * @return Return true to prevent any further processing.
+	             */
+	            public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
+					return true;
+	            	
+} }
+
