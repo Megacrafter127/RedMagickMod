778b79fa79d299f3402fe440fbd91e4ff8df9d8d
Matheus <matheus.de.alcantara@gmail.com>
2014-02-27 20:12:33 -0300

    Flying catalist HTT

--- src/com/matt/mod/magick/items/ItemFlyingCatalist.java
+++ src/com/matt/mod/magick/items/ItemFlyingCatalist.java
@@ -1,14 +1,23 @@
 package com.matt.mod.magick.items;
 
+import java.util.List;
+
+import org.lwjgl.input.Keyboard;
+
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.world.World;
 
 import com.matt.FutureCraft;
 import com.matt.lib.Ref;
+import com.matt.mod.generic.helpers.ChatHelper;
 import com.matt.mod.magick.items.logic.FlyingCatalistLogic;
 import com.matt.mod.magick.items.logic.ItemLogic;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemFlyingCatalist extends Item{
 public ItemFlyingCatalist() {
@@ -26,5 +35,13 @@ public boolean onItemUse(ItemStack itemStack, EntityPlayer entityPlayer, World w
 }
 public static ItemLogic getLogic() {
 	return new FlyingCatalistLogic();
+}
+@Override
+@SideOnly(Side.CLIENT)
+public void addInformation(ItemStack par1ItemStack,
+          EntityPlayer par2EntityPlayer,
+          List par3List,
+          boolean par4) {
+	 par3List.add(ChatHelper.htt("Flying, Because, Why not?", Keyboard.KEY_LSHIFT, new EnumChatFormatting[]{EnumChatFormatting.GOLD,EnumChatFormatting.UNDERLINE}));
 }
 }
