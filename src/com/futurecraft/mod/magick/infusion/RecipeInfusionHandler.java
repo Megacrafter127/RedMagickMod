081992dc73b9c5d26927006d3f87c7c7f0b91849
Matthew <matheus.de.alcantara@gmail.com>
2014-03-13 17:40:42 -0300

    Removed junk code.

--- src/com/futurecraft/mod/magick/infusion/RecipeInfusionHandler.java
+++
@@ -1,8 +0,0 @@
-package com.futurecraft.mod.magick.infusion;
-
-import java.util.HashMap;
-
-public class RecipeInfusionHandler {
-	HashMap<String,InfusionRecipe> recipeMap = new HashMap<String,InfusionRecipe>();
-	
-}
