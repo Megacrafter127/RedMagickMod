ac3d641f1eb0ed37c28faa65e5d28d342f7517ca
Megacrafter127 <player4596@web.de>
2014-03-24 14:21:48 +0100

    adding workshop textures

--- src/com/futurecraft/mod/witchery/block/BlockWorkshop.java
+++ src/com/futurecraft/mod/witchery/block/BlockWorkshop.java
@@ -1,6 +1,5 @@
 package com.futurecraft.mod.witchery.block;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
@@ -9,16 +8,15 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
+import com.futurecraft.mod.blocks.ColumnBlock;
 import com.futurecraft.mod.generic.helpers.ChatHelper;
 import com.futurecraft.mod.magick.alchemy.IManaContainer;
 import com.futurecraft.mod.magick.lib.MagickLib;
 
-public class BlockWorkshop extends Block implements ITileEntityProvider {
+public class BlockWorkshop extends ColumnBlock implements ITileEntityProvider {
 
 	public BlockWorkshop(int par1) {
-		super(par1, Material.wood);
-		super.setUnlocalizedName("Workshop");
-		super.setTextureName(MagickLib.toTextureName("Workshop"));
+		super(par1, Material.wood, MagickLib.toTextureName("workshop_top"), MagickLib.toTextureName("workshop_top"), MagickLib.toTextureName("workshop_side"), "Workshop");
 		super.setLightValue(1F);
 	}
 
@@ -29,7 +27,7 @@ public class BlockWorkshop extends Block implements ITileEntityProvider {
 	
 	@Override
 	public boolean onBlockActivated(World w,int x,int y,int z,EntityPlayer p,int side,float hitx,float hity,float hitz) {
-		System.out.println("Activated Workshop");
+		if(w.isRemote) return true;
 		TileEntityWorkshop t=(TileEntityWorkshop) w.getBlockTileEntity(x, y, z);
 		if(t.canSpend(p)) {
 			t.claim(p);
